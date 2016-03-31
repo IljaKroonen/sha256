08924005c9ca72c9403b110c7e8f4c0ac5805464
Ilja Kroonen <ilja.kroonen@gmail.com>
2016-03-31 22:49:04 +0200

    Added tests and fixed uint32-array-to-hex-string

--- test/all.ts
+++ test/all.ts
@@ -1,7 +1,9 @@
 import * as stringBlockToUint32Array from './string-block-to-uint32-array';
 import * as messageAppend from './message-append';
+import * as uint32ArrayToHexString from './uint32-array-to-hex-string';
 import * as sha256 from './sha256';
 
 stringBlockToUint32Array.test();
 messageAppend.test();
+uint32ArrayToHexString.test();
 sha256.test();
