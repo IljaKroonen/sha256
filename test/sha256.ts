abb0846f570910ad7137b82a3cea173ddb928407
Ilja Kroonen <ilja.kroonen@gmail.com>
2016-04-01 22:29:18 +0200

    Added test with longer message

--- test/sha256.ts
+++ test/sha256.ts
@@ -7,14 +7,23 @@ export function run() {
         sha256.add('');
         const actual = sha256.get();
         const expected = '0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';
-        test('Digest for empty string must be correct').expect(actual).toBe(expected);
+        test('digest for empty string must be correct').expect(actual).toBe(expected);
     })();
-    
+
     (function() {
         const sha256 = new Sha256(4);
         sha256.add('derp');
         const actual = sha256.get();
         const expected = '0x3f4146a1d0b5dac26562ff7dc6248573f4e996cf764a0f517318ff398dcfa792';
-        test('Digest for message "derp" must be correct').expect(actual).toBe(expected);
+        test('digest for message "derp" must be correct').expect(actual).toBe(expected);
+    })();
+
+    (function() {
+        const input = 'derpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderpderp';
+        const sha256 = new Sha256(input.length);
+        sha256.add(input);
+        const actual = sha256.get();
+        const expected = '0xdcf55796a9672c85f06cb988613c7d84025e5acdfdc609c32e18a5a7bb2067c1';
+        test('digest for message of length ' + input.length + ' must be correct').expect(actual).toBe(expected);
     })();
 }
