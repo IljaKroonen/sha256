dfeedd79ee1256fddcefa3e5c9fcea89ca3688b4
Ilja Kroonen <ilja.kroonen@gmail.com>
2016-04-05 21:34:46 +0200

    Minified version is now generated by closure compiler directly from the typescript source (closes #4)

--- sha256.ts
+++ sha256.ts
@@ -142,12 +142,19 @@
     }
 
     class Sha256 {
-        private addedLength = 0;
-        private buffer = '';
-        private hash = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19];
-        private scheduleArray = new Array(64);
+        private addedLength: number;
+        private buffer: string;
+        private hash: number[];
+        private scheduleArray: number[];
+        private length: number;
 
-        constructor(private length: number) { }
+        constructor(length: number) {
+            this.addedLength = 0;
+            this.buffer = '';
+            this.hash = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19];
+            this.scheduleArray = new Array(64);
+            this.length = length;
+        }
 
         add(part: string): void {
             this.addedLength += part.length;
